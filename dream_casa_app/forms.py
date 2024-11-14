66096d441d0b2f6d13fde1c2af9999a26a46ad62
asif <muhammedasifs321@gmail.com>
2024-11-15 01:23:05 +0530

    Render Deployement

---
+++ dream_casa_app/forms.py
@@ -0,0 +1,60 @@
+from django import forms
+from .models import ContactModel, NearByPlace, ClientReview, Gallery, Folder, Booking
+
+
+
+# Contact us
+class ContactModelForm(forms.ModelForm):
+    class Meta:
+        model = ContactModel
+        fields = '__all__'
+
+# NearByPlaceForm
+class NearByPlaceForm(forms.ModelForm):
+    class Meta:
+        model = NearByPlace
+        fields = '__all__'
+
+# Clien Review
+class ClientReviewForm(forms.ModelForm):
+    class Meta:
+        model = ClientReview
+        fields = '__all__'
+
+# Gallery
+class GalleryForm(forms.ModelForm):
+    class Meta:
+        model = Gallery
+        fields = '__all__'
+
+# Folder
+class FolderForm(forms.ModelForm):
+    class Meta:
+        model = Folder
+        fields = '__all__'
+
+
+# Booking
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = '__all__'  # You can choose specific fields if you don't need all fields
+        
+        # Define the custom widgets for arrival_date and departure_date
+        widgets = {
+            'arrival_date': forms.DateInput(
+                format='%A %d %B %Y',  # Full date format including weekday
+                attrs={'placeholder': 'Thursday 14 November 2024'}  # Optional: placeholder in the input field
+            ),
+            'departure_date': forms.DateInput(
+                format='%A %d %B %Y',  # Same date format for departure
+                attrs={'placeholder': 'Thursday 14 November 2024'}
+            ),
+        }
+        
+    # Ensuring that the form can handle the custom date format on submission
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Set the allowed input formats for both arrival_date and departure_date fields
+        self.fields['arrival_date'].input_formats = ['%A %d %B %Y']
+        self.fields['departure_date'].input_formats = ['%A %d %B %Y']
---
+++ dream_casa_app/urls.py
@@ -0,0 +1,68 @@
+from django.urls import path,include
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    # User Side
+    path('', views.index, name= 'index'),
+    path('about', views.about, name= 'about'),
+    path('contact', views.contact, name= 'contact'),
+    path('booking', views.booking, name= 'booking'),
+    path('rooms', views.rooms, name= 'rooms'),
+    path('room_details', views.room_details, name= 'room_details'),
+    path('near_by_places', views.near_by_places, name= 'near_by_places'),
+    path('near_by_place_details/<int:place_id>/', views.near_by_place_details, name='near_by_place_details'),
+    path('our_gallery', views.our_gallery, name= 'our_gallery'),
+    path('our_gallery', views.our_gallery, name= 'our_gallery'),
+    path('gallery/<int:folder_id>/', views.gallery, name= 'gallery'),
+
+
+
+      # Admin Login
+    path('login',views.user_login,name='user_login'),
+    path('logout_user', views.logout_user, name='logout_user'),
+
+    # admin dashboard
+    path('dashboard',views.dashboard,name='dashboard'),
+
+     # Contact us
+    path('contact_view',views.contact_view,name='contact_view'),
+    path('delete_contact/<int:id>/',views.delete_contact,name='delete_contact'),
+
+      # Booking
+    path('booking_view',views.booking_view,name='booking_view'),
+    path('delete_booking/<int:id>/',views.delete_booking,name='delete_booking'),
+
+
+    # NEar By Place
+    path('add_near_by_place', views.add_near_by_place, name='add_near_by_place'),
+    path('view_near_by_place',views.view_near_by_place,name='view_near_by_place'),
+    path('update_near_by_place/<int:id>/',views.update_near_by_place,name='update_near_by_place'),
+    path('delete_near_by_place/<int:id>/',views.delete_near_by_place,name='delete_near_by_place'),
+
+    path('ckeditor_upload/', views.ckeditor_upload, name='ckeditor_upload'),
+
+    # client reviews
+    path('add_client_review',views.add_client_review,name='add_client_review'),
+    path('view_client_reviews',views.view_client_reviews,name='view_client_reviews'),
+    path('update_client_review/<int:id>/',views.update_client_review,name='update_client_review'),
+    path('delete_client_review/<int:id>/',views.delete_client_review,name='delete_client_review'),
+
+     # Add Folders
+    path('add_folders', views.add_folders, name='add_folders'),
+    path('view_folders',views.view_folders,name='view_folders'),
+    path('update_folder/<int:id>/',views.update_folder,name='update_folder'),
+    path('delete_folder/<int:id>/',views.delete_folder,name='delete_folder'),
+
+     # Add Images
+    path('add_images', views.add_images, name='add_images'),
+    path('view_images',views.view_images,name='view_images'),
+    path('update_image/<int:id>/',views.update_image,name='update_image'),
+    path('delete_image/<int:id>/',views.delete_image,name='delete_image'),
+
+
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# handler404 = 'dream_casa_app.views.page_404'
